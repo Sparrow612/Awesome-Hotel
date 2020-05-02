b0589874089896e6bed63fcb37c46bdce0e1bc74
Cheng Rongxin <rongxinCRX@outlook.com>
2020-05-02 17:45:27 +0800

    update the proj by crx

---
+++ src/store/modules/hotelManager.js
@@ -0,0 +1,138 @@
+import {
+    addRoomAPI,
+    addHotelAPI,
+} from '@/api/hotelManager'
+import {
+    getAllOrdersAPI,
+} from '@/api/order'
+import {
+    hotelAllCouponsAPI,
+    hotelTargetMoneyAPI,
+} from '@/api/coupon'
+import {message} from 'ant-design-vue'
+
+const hotelManager = {
+    state: {
+        orderList: [],
+        addHotelParams: {
+            name: '',
+            address: '',
+            bizRegion: 'XiDan',
+            hotelStar: '',
+            rate: 0,
+            description: '',
+            phoneNum: '',
+            managerId: '',
+        },
+        addHotelModalVisible: false,
+        addRoomParams: {
+            roomType: '',
+            hotelId: '',
+            price: '',
+            total: 0,
+            curNum: 0,
+        },
+        addRoomModalVisible: false,
+        couponVisible: false,
+        addCouponVisible: false,
+        activeHotelId: 0,
+        couponList: [],
+    },
+    mutations: {
+        set_orderList: function (state, data) {
+            state.orderList = data
+        },
+        set_addHotelModalVisible: function (state, data) {
+            state.addHotelModalVisible = data
+        },
+        set_addHotelParams: function (state, data) {
+            state.addHotelParams = {
+                ...state.addHotelParams,
+                ...data,
+            }
+        },
+        set_addRoomModalVisible: function (state, data) {
+            state.addRoomModalVisible = data
+        },
+        set_addRoomParams: function (state, data) {
+            state.addRoomParams = {
+                ...state.addRoomParams,
+                ...data,
+            }
+        },
+        set_couponVisible: function (state, data) {
+            state.couponVisible = data
+        },
+        set_activeHotelId: function (state, data) {
+            state.activeHotelId = data
+        },
+        set_couponList: function (state, data) {
+            state.couponList = data
+        },
+        set_addCouponVisible: function (state, data) {
+            state.addCouponVisible = data
+        }
+    },
+    actions: {
+        getAllOrders: async ({state, commit}) => {
+            const res = await getAllOrdersAPI()
+            if (res) {
+                commit('set_orderList', res)
+            }
+        },
+        addHotel: async ({state, dispatch, commit}) => {
+            const res = await addHotelAPI(state.addHotelParams)
+            if (res) {
+                dispatch('getHotelList')
+                commit('set_addHotelParams', {
+                    name: '',
+                    address: '',
+                    bizRegion: 'XiDan',
+                    hotelStar: '',
+                    rate: 0,
+                    description: '',
+                    phoneNum: '',
+                    managerId: '',
+                })
+                commit('set_addHotelModalVisible', false)
+                await message.success('添加成功')
+            } else {
+                await message.error('添加失败')
+            }
+        },
+        addRoom: async ({state, dispatch, commit}) => {
+            const res = await addRoomAPI(state.addRoomParams) // res返回值是null
+            if (!res) { // 这段代码什么意思 返回的res为null才能正确提示？--crx
+                commit('set_addRoomModalVisible', false)
+                commit('set_addRoomParams', {
+                    roomType: '',
+                    hotelId: '',
+                    price: '',
+                    total: 0,
+                    curNum: 0,
+                }) // 为什么要进行这步覆盖操作？？？初始化？ -- crx 2020.04.24
+                await message.success('添加成功')
+            } else {
+                await message.error('添加失败')
+            }
+        },
+        getHotelCoupon: async ({state, commit}) => {
+            const res = await hotelAllCouponsAPI(state.activeHotelId)
+            if (res) {
+                commit('set_couponList', res)
+            }
+        },
+        addHotelCoupon: async ({commit, dispatch}, data) => {
+            const res = await hotelTargetMoneyAPI(data)
+            if (res) {
+                dispatch('getHotelCoupon')
+                commit('set_addCouponVisible', false)
+                commit('set_couponVisible', true)
+                await message.success('添加策略成功')
+            } else {
+                await message.error('添加失败')
+            }
+        }
+    }
+}
+export default hotelManager
