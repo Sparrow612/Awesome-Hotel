88bf9c7db2473a7614a043244e6c06a821a0f920
guo lihua <2366719611@qq.com>
2020-05-22 11:42:54 +0800

    feat: glh构建了网站营销人员的两个界面

---
+++ front_end/src/views/salesPerson/manageHotelMembership.vue
@@ -0,0 +1,17 @@
+<!-- created by glh 2020-05-22 -->
+<!-- 网站营销人员审核企业会员 -->
+<template>
+    <div>
+        <h1>网站营销人员的审核企业会员的界面</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "membership"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ front_end/src/views/salesPerson/siteCoupon.vue
@@ -0,0 +1,17 @@
+<!-- created by glh 2020-05-22 -->
+<!-- 网站营销人员的优惠券界面 -->
+<template>
+    <div>
+        <h1>网站营销人员的优惠券界面</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "siteCoupon"
+    }
+</script>
+
+<style scoped>
+
+</style>
